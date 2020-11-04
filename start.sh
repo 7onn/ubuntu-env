be2fe8d9ab06a91ef1f9e0a06a460ed1e32e63a0
tom <tom@localhost.localdomain>
2020-11-04 17:57:31 -0300

    feat(oh-my-zsh): add install at start.sh

--- start.sh
+++ start.sh
@@ -21,3 +21,10 @@ sudo yum install zsh
 
 echo 'exec zsh' >> ~/.bashrc
 
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+sudo ./aws/install
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
