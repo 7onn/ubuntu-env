e72ce2a697686ea2249deaffe49d070d8de010b2
tom <devbytom@gmail.com>
2020-12-21 16:50:35 -0300

    feat(.zshrc): add ansible

--- start.sh
+++ start.sh
@@ -65,3 +65,5 @@ sudo dnf install xclip xsel
 sudo dnf install -y dnf-plugins-core && \
     sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo && \
     sudo dnf -y install packer
+    
+sudo dnf install ansible
